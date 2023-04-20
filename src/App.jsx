e25525245fa374f8ae5817e894f2d89a2251751e
wAsI7 <wasi.sparkzzz@gmail.com>
2023-04-20 17:22:06 +0000

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
     return (
         <main>
-            React ⚛️ + Vite ⚡
+            Let's Make Tic Tac Toe Game in React!!!
         </main>
     )
 }
